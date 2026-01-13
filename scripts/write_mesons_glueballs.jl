e2d55b8ebe038a64877c36a54fb2c94a4018c14f
kzone-dev <nmrbrito2000@gmail.com>
2026-01-13 13:05:28 +0000

    branch that I used for parsing with my changes. Forcefully added input file for checking later.

--- scripts/write_mesons_glueballs.jl
+++ scripts/write_mesons_glueballs.jl
@@ -3,19 +3,18 @@ using HiRepParsing
 using HDF5
 using DelimitedFiles
 
-function main(listfile,h5file;setup=false,filter_channels=false,channels=nothing)
+function main(h5file;ensemble,disc,nhits,file,setup=true,filter_channels=false,channels=nothing)
     isfile(h5file) && rm(h5file)
-    for (ensemble,disc,nhits,file) in eachrow(readdlm(listfile,','))
-        if disc == "disc"
-            smearing_regex = r"DISCON_SEMWALL smear_N[0-9]+ SINGLET"
-            writehdf5_spectrum_disconnected_with_regexp(file,h5file,smearing_regex,nhits;mixed_rep=true,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
-        else
-            smearing_regex = r"source_N[0-9]+_sink_N[0-9]+ TRIPLET"
-            writehdf5_spectrum_with_regexp(file,h5file,smearing_regex;mixed_rep=true,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
-        end
-
+    if disc == "disc"
+        smearing_regex = r"DISCON_SEMWALL smear_N[0-9]+ SINGLET"
+        writehdf5_spectrum_disconnected_with_regexp(file,h5file,smearing_regex,nhits;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
+    else
+        smearing_regex = r"source_N[0-9]+_sink_N[0-9]+ TRIPLET"
+        writehdf5_spectrum_with_regexp(file,h5file,smearing_regex;mixed_rep=false,h5group=ensemble,setup,filter_channels,channels,sort=true,deduplicate=true)
     end
 end
 
-channels = readdlm("input/channels_glueballs.txt")
-main("input/listfile.txt","parsed_M3.hdf5",filter_channels=true,channels=channels)
+for (ensemble,rep,disc,nhits,file) in eachrow(readdlm("input/listfile.txt",','))
+    filename = ensemble * "_" * rep * "_" * disc * "_spectrum.hdf5"
+    main(filename;ensemble,disc,nhits,file,filter_channels=false,channels=nothing)
+end
